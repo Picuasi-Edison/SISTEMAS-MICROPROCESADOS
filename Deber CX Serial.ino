969787adbeb650fe47eca38869a1828ce19af038
Picuasi-Edison <44450696+Picuasi-Edison@users.noreply.github.com>
2018-11-13 10:55:36 -0500

    Update Deber CX Serial.ino

--- Deber CX Serial.ino
+++ Deber CX Serial.ino
@@ -1,48 +1,45 @@
-int tam,i=0,ntam;
-char opcion,vector1,vector2;
-String cedula,ncedula;
+//DEBER CX SERIAL
+// EDISON PICUASI
+
+int tam,i=0,j=0,ntam;
+char cedula[11],vector1,vector2;
+String ncedula;
 int voc=0;
-int boton=2;
 
 void setup() {
-Serial.begin(9600);
-pinMode(boton,INPUT);
+Serial.begin(9600); //inicializamos comunicacion serial
 Serial.println("OPCION:");
 Serial.println("1. Ingresar cedula: ");
-Serial.println("2. Validar cedula: ");
+
+//Ingreso de cedula
+for(;j<11;j){
+  if(Serial.available()>0){
+   cedula[j]=Serial.read();
+   j++;
+   Serial.println(cedula[j]);
+   
+
+    }
+  
+  }
 }
 
 void loop(){
+//leectura de datos en serial
   if(Serial.available()>0){
-  opcion=Serial.read();
-  }//recibe datos
-
-switch (opcion){
-  case 'A':
-  //Serial.println("1. Ingresar cedula: ");
-  if(Serial.available()>0){
-  cedula=Serial.readString();
-  tam=cedula.length();
-  char vector1[tam];
-  cedula.toCharArray(vector1,tam);
-  Serial.println(vector1[tam]);
-  }
-  break;
-  case 'B':
-  //Serial.println("2. Validar cedula: ");
-  if(Serial.available()>0){
-  ncedula=Serial.readString();
-  ntam=ncedula.length();
-  char vector2[ntam];
-  ncedula.toCharArray(vector2,ntam);
-  Serial.println(vector2[ntam]);
-  
+  ncedula=Serial.readString(); //asignamos los datos de serial a la variable
+  ntam=ncedula.length();//determinamos el tamano de la palabra
+  char vector1[ntam]; //creamos a un vector
+  ncedula.toCharArray(vector1,ntam);//asignamos los caracteres de la palabra al vector
+//Coomprobamos caracter por caracter  
   for(;i<ncedula.length();i++){
-      
-      }
+if(ncedula.charAt(i)==cedula[0]&&ncedula.charAt(i)==cedula[1]&&ncedula.charAt(i)==cedula[2]&&ncedula.charAt(i)==cedula[3]&&ncedula.charAt(i)==cedula[4]&&ncedula.charAt(i)==cedula[5]&&ncedula.charAt(i)==cedula[6]&&ncedula.charAt(i)==cedula[7]&&ncedula.charAt(i)==cedula[8]&&ncedula.charAt(i)==cedula[9]&&ncedula.charAt(i)==cedula[10]&&ncedula.charAt(i)==cedula[11]){        
+        Serial.println("CORRECTO"); //impresion de resultado
+      }else
+           
+        Serial.println("INCORRECTO");  //impresion de resultado
     }
   
-  break;
+ //Serial.println(ncedula);
   
-}
-  }
+}}
