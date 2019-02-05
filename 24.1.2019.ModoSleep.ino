6b4b5a87049130f38581a86d84d64a46b8539f87
Picuasi-Edison <44450696+Picuasi-Edison@users.noreply.github.com>
2019-02-05 09:47:02 -0500

    Update 24.1.2019.ModoSleep.ino

--- 24.1.2019.ModoSleep.ino
+++ 24.1.2019.ModoSleep.ino
@@ -1,36 +1,3 @@
-/*
-MODOS SLEEP
-Libreruas: <avr/power.h>
-             <avr/sleep.h>
-
-*Modos sleep
-*set_sleep_mode(mode);
-*                     mode: SLEEP_MODE_IDLE--->dormir la memoria del program
-*                           SLEEP_MODE_ADC--->Dormir adc
-*                           SLEEP_MODE_PWR_SAVE--->dormir memorias
-*                           SLEEP_MODE_STANDBY--->modo de ahorro
-*                           SLEEP_MODE_PWR_DOWN-->dormir al IC
-
-CONFIGURACIONES
-
-sleep_enable();--->habilita la entrada de modos sleep
-sleep_disable();--->deshabilita el modo sleep
-power_xx_enable/disable();
-                        power_adc_disable/enable();
-                        power_spi_disable/enable();
-                        power_txi_disable/enable();
-                        power_timer0_disable/enable();
-                        power_all_disable/enable();
-
-sleep_enable();
-sleep_disable();           
-
-
-en esta pagina esta todo lo referente a power sleep
-
-
-           
-*/
 #include <avr/power.h>
 #include <avr/sleep.h>
 #include <avr/wdt.h>
